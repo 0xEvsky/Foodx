1902b21248b1c5e6a1da7f8869fc32624900aa52
witsey <tubeakram7@gmail.com>
2025-05-10 12:42:12 +0300

    API: implement getting recipes JSON data when requesting GET at http://127.0.0.1:8000/recipes

--- backend/rf_backend/recipes/views.py
+++ backend/rf_backend/recipes/views.py
@@ -8,8 +8,9 @@ import json
 def recipes(request: HttpRequest):
     if request.method == 'GET':
         def getRecipes():
-            recipesSet = Recipe.objects.all().values()
+            recipesSet = Recipe.objects.all()
             recipesData = serializers.serialize('json', recipesSet)
             return HttpResponse(recipesData, content_type='application/json')
+        return getRecipes()
 
 
